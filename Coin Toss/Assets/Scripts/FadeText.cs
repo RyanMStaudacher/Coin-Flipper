532f3a32486c3af121a215d6d29a929b47efaa19
Ryan Staudacher <ryanmstaudacher@gmail.com>
2018-08-24 15:33:45 -0500

    Adds ads and text fader script

---
+++ Coin Toss/Assets/Scripts/FadeText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FadeText : MonoBehaviour
+{
+    [SerializeField] private float waitTime = 1.0f;
+    [SerializeField] private float fadeSpeed = 1.0f;
+
+    private Text text;
+
+	// Use this for initialization
+	void Start ()
+    {
+		if(GetComponent<Text>() != null)
+        {
+            text = GetComponent<Text>();
+        }
+
+        StartCoroutine(FadeTextTimer());
+	}
+
+    IEnumerator FadeTextTimer()
+    {
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(FadeTheText());
+    }
+
+    IEnumerator FadeTheText()
+    {
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / fadeSpeed));
+            yield return null;
+        }
+    }
+}
